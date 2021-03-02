a142b028ad7fe3b5f9d5a0a2cdad337d52cdad6f
lekan <lekan.lekan@gmail.com>
2021-03-02 14:00:56 +0300

    clockface

---
+++ clockface/clockface.go
@@ -0,0 +1,31 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func SecondHand(t time.Time) Point {
+	p := secondHandPoint(t)
+	p = Point{p.X * 90, p.Y * 90}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + 150, p.Y + 150}
+	return p
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
